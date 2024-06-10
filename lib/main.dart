da5c6d5944251ff1637885e03ed5395b872b945d
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-06-10 09:14:38 +0530

    json file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +34,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List sList=[];
+  List sList = [];
+  TextEditingController name = TextEditingController();
+  TextEditingController mobile = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // getDataFormSp();
+  }
+
+  void getDataFormSp() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String cl = prefs.getString("cl") ?? "";
+
+    Map<String, dynamic> jsonData = jsonDecode(cl);
+    sList = jsonData["contact_list"] ?? [];
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,28 +60,62 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-        itemCount: sList.length,
-        itemBuilder: (context, index) {
-          Map<String,dynamic> item = sList[index];
+      body: Column(
+        children: [
+          TextFormField(
+            controller: name,
+            decoration: InputDecoration(hintText: "Name"),
+          ),
+          TextFormField(
+            controller: mobile,
+            decoration: InputDecoration(hintText: "Mobile Number"),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              Map<String, dynamic> map = {
+                "name": name.text,
+                "mobile_number": mobile.text,
+              };
+              sList.add(map);
+              setState(() {});
 
-          return ListTile(
-            title: Text(item["name"]??""),
-            subtitle: Text("age : ${item["age"]}"),
-          );
-        },
+              Map<String, dynamic> contactList = {"contact_list": sList};
+
+              String encodeString = jsonEncode(contactList);
+              final SharedPreferences prefs = await SharedPreferences.getInstance();
+              prefs.setString("cl", encodeString);
+              name.clear();
+              mobile.clear();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Conctact Added")));
+            },
+            child: Text("Ok"),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: sList.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> item = sList[index];
+
+                return ListTile(
+                  title: Text(item["name"] ?? ""),
+                  subtitle: Text("${item["mobile_number"]}"),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          String data =
-              '{"status":true,"message":"success","student":[{"name":"s1","age":10,"address":"rajkot"},{"name":"jayveer","age":18,"address":"150 ring road,rajkot"},{"name":"s5","age":10,"address":"rajkot"},{"name":"new jayveer","age":18,"address":"150 ring road,rajkot"}]}';
-          Map<String, dynamic> jsonData = jsonDecode(data);
-          sList = jsonData["student"]??[];
-          setState(() {
+        onPressed: () async{
+          // String data =
+          //     '{"contact_list":[{"name":"jaybeer","mobile_number":464647687},{"name":"veer","mobile_number":464647687},{"name":"harsh","mobile_number":47978},{"name":"baby","mobile_number":455456464}]}';
 
-          });
-          
-          rootBundle.loadString(key)
+          String data=await rootBundle.loadString("assets/contact.json");
+          print("data $data");
+
+          Map<String, dynamic> jsonData = jsonDecode(data);
+          sList = jsonData["contact_list"] ?? [];
+          setState(() {});
 
           print(sList);
         },
