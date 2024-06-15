f5121b79c038965c75a2fc70cf4ca796b761e4af
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-06-15 10:23:58 +0530

    pixabay api with category

--- lib/pixabay_api.dart
+++ lib/pixabay_api.dart
@@ -1,6 +1,11 @@
-import 'dart:convert';
+// ignore_for_file: use_build_context_synchronously
 
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:async_wallpaper/async_wallpaper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:http/http.dart';
 import 'package:json_demo/ApiHelper.dart';
 import 'package:json_demo/pixabay_model.dart';
@@ -38,8 +43,9 @@ class _PixabayApiState extends State<PixabayApi> {
   ];
 
   int selectedcategoryIndex = 0;
+
   // String? selectedImgType;
-  int selectedImgTypeIndex=0;
+  int selectedImgTypeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +78,7 @@ class _PixabayApiState extends State<PixabayApi> {
               );
             }),
             onChanged: (value) {
-              selectedImgTypeIndex = value??0;
+              selectedImgTypeIndex = value ?? 0;
               setState(() {});
               print(value);
             },
@@ -91,7 +97,7 @@ class _PixabayApiState extends State<PixabayApi> {
                   padding: const EdgeInsets.all(8.0),
                   child: ActionChip(
                     label: Text(category[index]),
-                    padding: EdgeInsets.all(5),
+                    padding: const EdgeInsets.all(5),
                     backgroundColor: selectedcategoryIndex == index ? Colors.red : Colors.blueGrey,
                     onPressed: () {
                       selectedcategoryIndex = index;
@@ -119,22 +125,91 @@ class _PixabayApiState extends State<PixabayApi> {
                     ),
                     itemBuilder: (context, index) {
                       var hit = pixaBayModel.hits![index];
-                      return Container(
-                        child: Image.network(
-                          hit.largeImageUrl ?? "",
-                          fit: BoxFit.cover,
+                      return InkWell(
+                        onTap: () async {
+                          File file = await DefaultCacheManager().getSingleFile(hit.largeImageUrl ?? "");
+
+                          // bool isSuccess = await AsyncWallpaper.setWallpaperFromFile(
+                          //   filePath: file.path,
+                          //   goToHome:false,
+                          //   wallpaperLocation: AsyncWallpaper.BOTH_SCREENS,
+                          // );
+
+                          var res = await showModalBottomSheet(
+                            context: context,
+                            builder: (context) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                                  children: [
+                                    InkWell(
+                                      onTap: () async {
+                                        Navigator.pop(context,AsyncWallpaper.LOCK_SCREEN);
+                                      },
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Text("Lock Scren"),
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: () async {
+                                        Navigator.pop(context,AsyncWallpaper.HOME_SCREEN);
+
+                                      },
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Text("Home Scren"),
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: () {
+                                        Navigator.pop(context,AsyncWallpaper.BOTH_SCREENS );
+                                      },
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Text("Both Scren"),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+
+                          if(res!=null){
+                            setAppWallpaper(res,file);
+                          }
+
+
+                          // print("object--- $isSuccess");
+                        },
+                        child: Container(
+                          child: Image.network(
+                            hit.largeImageUrl ?? "",
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       );
                     },
                   );
                 } else {
-                  return CircularProgressIndicator();
+                  return const CircularProgressIndicator();
                 }
               },
             ),
           ),
         ],
       ),
+    );
+  }
+
+  void setAppWallpaper(int type, File file) async {
+    bool isSuccess = await AsyncWallpaper.setWallpaperFromFile(
+      filePath: file.path,
+      goToHome: true,
+      wallpaperLocation: type,
     );
   }
 }
